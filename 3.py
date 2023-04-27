411a2550d0ec31b6268f72848a9ec084ca1ac640
devopsfaisal <clumsyfaisal@gmail.com>
2023-04-27 18:10:35 +0530

    Swapping of two numbers using Python Feature

---
+++ 3.py
@@ -0,0 +1,8 @@
+# Swapping of two numbers using Python Feature.
+
+a = int(input("Enter first number = "))
+b = int(input("Enter first number = "))
+
+a,b = b,a
+
+print("Value of a after swapping = %d \nValue of b after swapping = %d" %(a,b))
