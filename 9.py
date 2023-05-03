57b2684b9f3b52015b142005dc2cac6b2d03d690
devopsfaisal <clumsyfaisal@gmail.com>
2023-05-03 13:03:30 +0530

    Print Factorial of any Number

--- 9.py
+++ 9.py
@@ -3,10 +3,10 @@
 num = int(input("Enter the number whose factorial to print = "))
 
 def fact(num):
-    result = 1
-    while num>=1:
-        result=result*num
-        num=num-1
-    return result
+    fact_num = 1
+    while num >= 1:
+        fact_num = fact_num * num
+        num = num - 1
+    return fact_num
 
 print('Ther Factorial of {} is {}'.format(num, fact(num)))
