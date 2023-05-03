f3114caad5b6391b9fc8f6d0e2df1dd44d3c517e
devopsfaisal <clumsyfaisal@gmail.com>
2023-05-03 13:10:50 +0530

    Print Factorial of Series of Numbers

--- 10.py
+++ 10.py
@@ -1,5 +1,5 @@
 # Print Factorial of Series of Numbers
-# eg Factorial of 1 to 10
+# eg Factorial of 0 to 10
 
 num = int(input("Enter the number whose factorial to print = "))
 
@@ -10,5 +10,5 @@ def fact(num):
         num = num - 1
     return fact_num
 
-for i in range(num+1):
-    print('Ther Factorial of {} is {}'.format(num, fact(num)))
+for i in range(num):
+    print('Ther Factorial of {} is {}'.format(i+1, fact(i+1)))
