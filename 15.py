35e8b1882867d779db09ec83cdb2c6b7144d9fb2
devopsfaisal <clumsyfaisal@gmail.com>
2023-05-03 18:45:19 +0530

    Use of filter() using lambda

---
+++ 15.py
@@ -0,0 +1,10 @@
+# We can pass function as argument to another function, for this lambda function is best option.
+# function1(function2, sequence)
+# filter() can be used to filter values from the given sequence based on some conditions
+# filter() function Example
+
+list1 = [0,5,10,15,20,25,30,35,40]
+
+# As type of list2 variable is filter so we have to typecast list2 in list using list()
+list2 = list(filter(lambda num : num%2==0, list1))
+print(list2)
