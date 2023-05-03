55522b2aad11ee4b89d649aec826cfa67505e937
devopsfaisal <clumsyfaisal@gmail.com>
2023-05-03 18:50:52 +0530

    Use of filter() using lambda

--- 15.py
+++ 15.py
@@ -8,3 +8,6 @@ list1 = [0,5,10,15,20,25,30,35,40]
 # As type of list2 variable is filter so we have to typecast list2 in list using list()
 list2 = list(filter(lambda num : num%2==0, list1))
 print(list2)
+
+list3 = list(filter(lambda num : num%2!=0, list1))
+print(list3)
